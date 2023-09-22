501d3fc6371dcadcfc31e9f811c8e6e244e06316
Shahid Parvez <shahidparvez.official@gmail.com>
2023-09-22 12:14:15 +0600

    disable function bundling

--- courseReg/vite.config.js
+++ courseReg/vite.config.js
@@ -1,7 +1,28 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { createHtmlPlugin } from 'vite-plugin-html'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    createHtmlPlugin({
+      minify: true,
+      /**
+       * After writing entry here, you will not need to add script tags in `index.html`, the original tags need to be deleted
+       * @default src/main.ts
+       */
+      // entry: 'src/main.ts',
+      /**
+       * If you want to store `index.html` in the specified folder, you can modify it, otherwise no configuration is required
+       * @default index.html
+       */
+      // template: 'public/index.html',
+
+      /**
+       * Data that needs to be injected into the index.html ejs template
+       */
+      
+    }),
+  ],
 })
